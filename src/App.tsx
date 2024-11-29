fbb14d739c29ab5173adefd6552a1cf7f3db07fc
CanerYesiltas <caneryesiltas1@gmail.com>
2024-11-29 15:52:50 -0500

    feat: Implement type-safe input handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
+import { useState, ChangeEvent } from 'react';
 import './styles/App.css';
 
-function App() {
-  return <div></div>;
-}
+const App = () => {
+  const [task, setTask] = useState<string>('');
+  const [workDay, setWorkDay] = useState<number>(0);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.name === 'task') {
+      setTask(event.target.value);
+    } else {
+      setWorkDay(Number(event.target.value));
+    }
+    console.log(task, workDay);
+  };
+  return (
+    <div className='App'>
+      <input
+        type='text'
+        value={task}
+        name='task'
+        placeholder='Ready to make things happen?'
+        onChange={handleChange}
+      />
+      <input
+        type='number'
+        value={workDay}
+        name='workDay'
+        placeholder='Set your deadline, crush your goals!'
+        onChange={handleChange}
+      />
+      <button>Launch Task</button>
+    </div>
+  );
+};
 
 export default App;
